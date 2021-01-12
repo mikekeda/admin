700cbe40302365578f05913fcf8ecf5b5bac7632
Mykhailo Keda <mriynuk@gmail.com>
2021-01-12 14:46:18 +0000

    Created run.py

--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-import socket
 import uuid
 from functools import wraps
 from typing import Tuple
@@ -8,7 +7,6 @@ from typing import Tuple
 import aiofiles
 from aiohttp import ClientConnectorError, ClientSession
 from sanic.exceptions import abort
-from sanic.log import logger
 from sanic.response import html
 from sanic.response import json as sanic_json
 from sanic.response import redirect
@@ -153,22 +151,3 @@ class LoginView(HTTPMethodView):
 
 
 app.add_route(LoginView.as_view(), '/login')
-
-
-if __name__ == "__main__":
-    if app.config['DEBUG']:
-        app.run(host="0.0.0.0", port=8000, debug=True)
-    else:
-        # Remove old socket (is any).
-        try:
-            os.unlink(app.config['SOCKET_FILE'])
-        except FileNotFoundError as e:
-            logger.info(f"No old socket file found: {e}")
-
-        # Create socket and run app.
-        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
-            try:
-                sock.bind(app.config['SOCKET_FILE'])
-                app.run(sock=sock, access_log=False)
-            except OSError as e:
-                logger.warning(e)
