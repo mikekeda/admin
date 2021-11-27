5c764275572e2dc2ac792c8105734afdd026e56b
Mykhailo Keda <mriynuk@gmail.com>
2021-11-27 11:13:29 +0000

    Added available updates tooltips.

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     let elems = document.querySelectorAll(".dropdown-trigger");
     M.Dropdown.init(elems);
+    let tooltips = document.querySelectorAll(".tooltipped");
+    M.Tooltip.init(tooltips);
 });
